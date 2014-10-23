a132d7cbb533001f5a8f8bf25ffa4df9412b3d4c
Chang Liu <3dslayer@gmail.com>
2014-10-23 23:22:02 +1100

    Optimize the layout

--- MyTestApp/js/controllers.js
+++ MyTestApp/js/controllers.js
@@ -37,32 +37,32 @@ angular.module('starter.controllers', [])
         $scope.showSearch = function ($event) {
             $scope.isSeachShow = !$scope.isSeachShow;
         };
-        $ionicPopover.fromTemplateUrl('my-popover.html', {
-            scope: $scope,
-        }).then(function (popover) {
-            $scope.popover = popover;
-        });
-        $scope.openPopover = function ($event) {
-            $scope.popover.show($event);
-        };
-        $scope.closePopover = function () {
-            $scope.popover.hide();
-        };
-        //Cleanup the popover when we're done with it!
-        $scope.$on('$destroy', function () {
-            $scope.popover.remove();
-        });
-        // Execute action on hide popover
-        $scope.$on('popover.hidden', function () {
-            // Execute action
-        });
-        // Execute action on remove popover
-        $scope.$on('popover.removed', function () {
-            // Execute action
-        });
-        $scope.doScroll = function() {
-            console.log('gaga');
-        }
+//        $ionicPopover.fromTemplateUrl('my-popover.html', {
+//            scope: $scope,
+//        }).then(function (popover) {
+//            $scope.popover = popover;
+//        });
+//        $scope.openPopover = function ($event) {
+//            $scope.popover.show($event);
+//        };
+//        $scope.closePopover = function () {
+//            $scope.popover.hide();
+//        };
+//        //Cleanup the popover when we're done with it!
+//        $scope.$on('$destroy', function () {
+//            $scope.popover.remove();
+//        });
+//        // Execute action on hide popover
+//        $scope.$on('popover.hidden', function () {
+//            // Execute action
+//        });
+//        // Execute action on remove popover
+//        $scope.$on('popover.removed', function () {
+//            // Execute action
+//        });
+//        $scope.doScroll = function() {
+//            console.log('gaga');
+//        }
         $scope.doRefresh = function() {
 //            $scope.playlists.push({ title: 'agag', id: 7 });
             $scope.$broadcast('scroll.refreshComplete');
